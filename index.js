ac43201adcc9a87f5dd0c27bf43261cf01d95263
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-02-17 15:04:31 +0200

    exercises1

---
+++ index.js
@@ -0,0 +1,14 @@
+//EXERCISE 1:
+const productCategories = ["book", "electronics", "clothing", "home& kitchen", "toys & Games",];
+// a
+console.log(productCategories[0]);
+// b
+console.log(productCategories[productCategories.length - 1]);
+// c
+console.log(productCategories.length);
+// d
+productCategories[1] = "pen";
+// e
+console.log(productCategories);
+
+//EXERCISE 2
