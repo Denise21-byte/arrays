7bad8702a011209f66b2b7839f0ab98049bdc472
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-02-17 15:23:50 +0200

    exercise2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//EXERCISE 1:
+//EXERCISE 1:Array Initialization and Property 
 const productCategories = ["book", "electronics", "clothing", "home& kitchen", "toys & Games",];
 // a
 console.log(productCategories[0]);
@@ -11,4 +11,29 @@ productCategories[1] = "pen";
 // e
 console.log(productCategories);
 
-//EXERCISE 2
+//EXERCISE 2 :push, pop, shift, unshift, splice
+let inventory = [10,20,30];
+
+// a
+inventory.push(40);
+console.log(inventory);
+
+// b
+inventory.pop();
+console.log(inventory);
+
+// c
+inventory.unshift(0);
+console.log(inventory);
+
+// d
+inventory.shift();
+console.log(inventory);
+
+// e
+inventory.splice(1, 1, 25, 35);
+//a
+console.log(inventory);
+
+
+
